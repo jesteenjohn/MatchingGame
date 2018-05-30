ed823b9c938be05356cd2b1b32786e1c3ff29199
Jesteen John <justin.mailme@gmail.com>
2018-05-30 16:07:54 -0500

    Generated card deck.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
-/*
- * Create a list that holds all of your cards
- */
 
+let clicked = 1;
+let previousClickedImageCssValue="";
+let previousClickedImage = null;
+let startTime = null;
+let moves = 0;
+let starRemove=32;
+/*
+ * @description Create a list that holds all of your cards.
+ * @returns {array} Array of css classnames for all 16 cards.
+*/
+function listOfCards()
+{
+    const cards =  [
+    "card fa fa-diamond", "card fa fa-paper-plane-o", "card fa fa-anchor",
+    "card fa fa-bolt", "card fa fa-cube", "card fa fa-anchor", "card fa fa-leaf", "card  fa fa-bicycle",
+    "card fa fa-diamond", "card fa fa-bomb", "card fa fa-leaf", "card fa fa-bomb", "card fa fa-bolt",
+    "card fa fa-bicycle", "card fa fa-paper-plane-o", "card fa fa-cube"
+    ];
+
+    return cards;
+}
 
 /*
- * Display the cards on the page
+ * @description Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ * @param {array} array
+ * @returns {array} Shuffled array
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -27,7 +47,7 @@ function shuffle(array) {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
+ * @description set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
@@ -36,3 +56,55 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+document.addEventListener('DOMContentLoaded',OnLoad);
+
+/*
+*@description This is the first function that is called after the DOM is loaded.
+*/
+function OnLoad()
+{
+    generateCardDeck();
+    addListenersToCardsInDeck();
+    const restartElement = document.getElementsByClassName("fa-repeat")[0];
+    restartElement.addEventListener('click',restartGame);
+}
+
+/*
+*@description Generates the deck of cards
+*/
+function generateCardDeck()
+{
+    const cards = listOfCards();
+    const shuffledCards = shuffle(cards);
+    let documentFragment =  document.createDocumentFragment();
+
+    shuffledCards.forEach((cardClassNames, index) => {
+        const listElement = document.createElement('li');
+        listElement.className = cardClassNames;
+        documentFragment.appendChild(listElement);
+    });
+
+    const deck = document.getElementById("deck-container");
+    deck.appendChild(documentFragment);
+}
+
+/*
+*@description Registers click event listeners to all the card elements.
+*/
+function addListenersToCardsInDeck()
+{
+    const lis = document.getElementById("deck-container").getElementsByTagName('li');
+
+    for (let i=0; i<lis.length; i++) {
+        lis[i].addEventListener('click', cardClickWorkflow, false);
+    }
+}
+
+/*
+*@description List of events triggered after a card is clicked.
+*/
+function cardClickWorkflow(event) {
+   
+}
